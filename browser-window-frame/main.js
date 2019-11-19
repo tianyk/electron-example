727ddf096bcbfaaf7076e4e8a9c339c21b66b3a4
tyk <yongketian@gmail.com>
2019-11-19 11:00:45 +0800

    frame window

--- browser-window-frame/main.js
+++ browser-window-frame/main.js
@@ -6,14 +6,19 @@ const createWindow = require('./src/create_window');
 
 app.on('ready', () => {
 	const win = createWindow('view/index.html', {
+		width: 513,
+		height: 342,
 		// titleBar 隐藏后将无法拖动
-		// titleBarStyle: 'hidden',
+		titleBarStyle: 'hidden',
+		// 不能改变大小
+		resizable: false,
+		// 禁用最大最小化
+		minimizable: false,
+		maximizable: false,
 		// frame 创建一个无标题栏和窗口控制按钮的窗口
 		frame: false,
 		webPreferences: {
-			devTools: false,
-			// 先与网页脚本执行，运行环境在 BrowserWindow 中
-			preload: path.join(__dirname, 'preload.js')
+			devTools: false
 		}
 	});
 });
