c95147da4a5433d3108c3d795d7efc20e8a2dd49
tyk <yongketian@gmail.com>
2019-11-19 22:28:14 +0800

    BrowserWindow

--- browser-window/main.js
+++ browser-window/main.js
@@ -6,12 +6,7 @@ const createWindow = require('./src/create_window');
 
 app.on('ready', () => {
 	const win = createWindow('view/index.html', {
-		// titleBar 隐藏后将无法拖动
-		// titleBarStyle: 'hidden',
-		// frame 创建一个无标题栏和窗口控制按钮的窗口
-		frame: false,
 		webPreferences: {
-			devTools: false,
 			// 先与网页脚本执行，运行环境在 BrowserWindow 中
 			preload: path.join(__dirname, 'preload.js')
 		}
